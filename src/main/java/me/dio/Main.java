8906eb1bb80149111da1176d8ca748e2e2739892
Thiago Poiani <thpoiani@gmail.com>
2023-02-09 12:19:59 -0700

    Bootstrap: Main method - Launch it directly from the IDE without needing to run a Maven

---
+++ src/main/java/me/dio/Main.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+
+    public static void main(String ... args) {
+        System.out.println("Running main method");
+        Quarkus.run(args);
+    }
+}
